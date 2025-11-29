742b60c26d17dbb4374d220ede2d820a40e814ca
Caleb <caleb.hunt789@gmail.com>
2025-11-29 04:52:58 +0000

    ran out of time

--- lab-e/js/current-page-nav.js
+++ lab-e/js/current-page-nav.js
@@ -1,9 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
 // STEP C: Get the current URL for the page
 
 // STEP D: Grab all the top-level <a> elements inside the <nav> element at the top of the page (inside the <header> element)
-
+const navLinks = document.querySelectorAll("header nav > ul > li > a")
 // STEP E: Create a place to store the href value for each <a> element
-
+let linkHref;
 // STEP F: For each <a> element in the NodeList navLinks, compare the href property with the URL for the current page
 
     // STEP G: Grab the href property of each <a> (element)
